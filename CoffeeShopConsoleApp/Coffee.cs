875c1d62cd6be34a2a71474d37038b2c3afd95c2
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2019-09-03 21:40:27 +0200

    Add project files.

---
+++ CoffeeShopConsoleApp/Coffee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoffeeShopConsoleApp
+{
+    /// <summary>
+    /// A class that is used for coffe orders
+    /// </summary>
+    public class Coffee
+    {
+        /// <summary>
+        /// returns the price of the coffee
+        /// It's possible to override this method, beacuse it is virtual 
+        /// </summary>
+        /// <returns>20 dkr</returns>
+        public virtual int price()
+        {
+            return 20;
+        }
+
+    }
+}
---
+++ CoffeeShopConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoffeeShopConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Coffee Shop!");
+
+            // a list of all the orders for coffe in the coffee shop
+            List<Coffee> orderList = new List<Coffee>()
+            {
+                new Coffee(),
+                new Coffee(),
+                new Coffee()
+
+            };
+
+
+            foreach (var coffee in orderList)
+            {
+                Console.WriteLine("the Price of the coffee is : "+ coffee.price());
+            }
+
+
+        }
+    }
+}
