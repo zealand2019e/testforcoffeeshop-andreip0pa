9c257b4f6652af2112c015e220fe1ea1e2a6b78a
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2019-09-10 17:29:16 +0200

    added FlatWhite class

---
+++ CoffeeShopConsoleApp/FlatWhite.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoffeeShopConsoleApp
+{
+    public class FlatWhite : Coffee
+    {
+        public FlatWhite()
+            :base()
+        {
+        }
+
+        public FlatWhite(int discount) 
+            : base(discount)
+        {
+        }
+
+        public override string Strength()
+        {
+            return "Medium";
+        }
+
+        public override int price()
+        {
+            return 35 - Discount;
+        }
+    }
+}
