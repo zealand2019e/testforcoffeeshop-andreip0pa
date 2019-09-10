63313e482e0820f2e0a408cb6ba08296538c9d55
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2019-09-10 14:38:44 +0200

    added BlackCoffee class

---
+++ CoffeeShopConsoleApp/BlackCoffee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoffeeShopConsoleApp
+{
+    public class BlackCoffee : Coffee
+    {
+        public BlackCoffee()
+        {
+        }
+    }
+}
