65c1edfea0095f72d92650250d8cbcb88f3c84a0
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2019-09-10 14:40:48 +0200

    Added Latte class

---
+++ CoffeeShopConsoleApp/Latte.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoffeeShopConsoleApp
+{
+    public class Latte : Coffee
+    {
+        public Latte()
+        {
+        }
+
+        public override int price()
+        {
+            return 40;
+        }
+    }
+}
