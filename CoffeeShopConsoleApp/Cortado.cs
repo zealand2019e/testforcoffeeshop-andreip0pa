185825021eeb18d85902a88c67f0f3e461acc72e
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2019-09-10 14:41:47 +0200

    added Cortado class

---
+++ CoffeeShopConsoleApp/Cortado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoffeeShopConsoleApp
+{
+    public class Cortado : Coffee
+    {
+        public Cortado()
+        {
+        }
+
+        public override int price()
+        {
+            return 25;
+        }
+
+    }
+}
