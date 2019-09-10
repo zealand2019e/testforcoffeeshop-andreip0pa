d9f7d640bbef9cf36920497141fca7c585050512
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2019-09-10 17:33:01 +0200

    Added interface ICoffeeBlend  --method string CoffeBlend()

---
+++ CoffeeShopConsoleApp/ICoffeeBlend.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoffeeShopConsoleApp
+{
+    interface ICoffeeBlend
+    {
+        string CoffeeBlend();
+    }
+}
